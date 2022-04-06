c9e980aa2ddf027a06d55f564957bb23a9c5d671
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-04-06 18:59:35 +0200

    head + icone info

--- app/extension.php
+++ app/extension.php
@@ -2,4 +2,6 @@
 include "env.php";
 
 require_once $env_urlFile . "Boostrap.phtml";
+require_once $env_urlFile . "GoogleFront.phtml";
+require_once $env_urlFile . "FontAwasome.phtml";
 require_once $env_urlFile . "link.phtml";
--- index.php
+++ index.php
@@ -3,7 +3,77 @@
 
 <main>
 
-    <p>Bienvenue</p>
+    <section id="home">
+
+        <div class="head">
+            <div class="centertext">
+                <h1>L'hébergeur de confiance créé par des professionels du secteur.   </h1>
+                <h3>
+                    Parce que vos projets méritent d'être réalisés <br>
+                    avec le savoir-faire des équipes de <a target="_blank" href="https://tyrolium.fr">Tyrolium</a>.
+                </h3>
+            </div>
+        </div>
+
+    </section>
+
+
+
+    <section id="bull">
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-4">
+                    <h3><i class="fas fa-bolt"></i> PUISSANCE</h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+                <div class="col-4">
+
+                    <h3><i class="fas fa-handshake"></i> TRANSPARENCE/ CONFIANCE</h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+                <div class="col-4">
+
+                    <h3><i class="fas fa-trophy"></i> EXPERIENCE</h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+                <div class="col-4">
+
+                    <h3><i class="fas fa-tools"></i> SUPPORT</h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+                <div class="col-4">
+                    <h3><i class="fas fa-shield-alt"></i> FIABILITE </h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+                <div class="col-4">
+                    <h3><i class="fas fa-thumbs-up"></i> SATISFACTION</h3>
+
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+
+
+
+
+
+
 
 </main>
 
