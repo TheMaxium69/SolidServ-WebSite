19ff5c30e43fe1c8f15c4f8ecb4c210d3d934060
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-04-09 18:04:58 +0200

    whmcs update

--- app/env.php
+++ app/env.php
@@ -8,7 +8,7 @@ $APP_ENV = "PROD";
 if ($APP_ENV == "DEV"){
     $env_whmcs = "http://localhost/whmcs/whmcs-8.2.1/whmcs-8.2.1/";
 } else if ($APP_ENV == "PROD"){
-    $env_whmcs = "http://vps202.tyrolium.fr/SolidServ-WebServ/panel/";
+    $env_whmcs = "http://vps202.tyrolium.fr/SolidServ-WebSite/panel/";
 }
 
 $env_urlFile = "./extension/";
