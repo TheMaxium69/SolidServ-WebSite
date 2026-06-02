a5f5ae07122c3c809e8a02a7519014df54bcd523
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-02 19:07:54 +0200

    theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TYRO_RELAY_URL, TYRO_THEME_RELAY_URL } from 'tyrolium-ui';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    {
+      provide: TYRO_RELAY_URL,
+      useValue: isDevMode()
+        ? 'http://localhost:9001/theme-relay.html'  // tyrolium-website doit tourner sur :9001mai
+        : TYRO_THEME_RELAY_URL,
+    },
+  ],
 };
