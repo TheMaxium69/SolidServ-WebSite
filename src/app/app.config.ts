9cdf842f252cb58998c446a922e23f7463126f5a
TheMaximeSan <mxm.tournier@gmail.com>
2026-06-22 20:30:29 +0200

    delete relay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
-import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import {provideRouter, withInMemoryScrolling} from '@angular/router';
-import { TYRO_RELAY_URL, TYRO_RELAY_PAGE_URL } from 'tyrolium-ui';
 
 import { routes } from './app.routes';
 
@@ -11,12 +10,6 @@ export const appConfig: ApplicationConfig = {
         withInMemoryScrolling({
           scrollPositionRestoration: 'enabled',
           anchorScrolling: 'enabled'
-        })),
-    {
-      provide: TYRO_RELAY_URL,
-      useValue: isDevMode()
-        ? 'http://192.168.1.81:9001/relay.html'  // port de tyrolium-website en dev
-        : TYRO_RELAY_PAGE_URL,
-    },
+        }))
   ],
 };
